e026670339f98a61a44bfa1dd7208a3d535c8ec7
smilesnail <app_!QAZxsw20>
2024-06-16 21:02:10 +0800

    初始化项目

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const resolve = (p: string) => {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/bellsscan/', // github仓库名称
     resolve: {
         alias: {
             '@': resolve('./src')
